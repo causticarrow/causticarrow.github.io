5a7b703b419a6c54e7cda4b9c894dbf73b7753c2
causticarrow <rarementx@gmail.com>
2026-08-01 09:08:50 -0400

    Update print statement from 'Hello' to 'Goodbye'

--- raw-picks.js
+++ raw-picks.js
@@ -1,5 +1,6 @@
 window.RAW_PICKS = [
-  { date: "2026-08-01", tournament: "BLAST Bounty", play: "FaZe +1.5 vs. Spirit", odds: +115, score: "", game: "CS2"},
+  { date: "2026-08-01", tournament: "BLAST Bounty", play: "Astralis +1.5 vs. MOUZ", odds: -140, score: "", game: "CS2"},
+  { date: "2026-08-01", tournament: "BLAST Bounty", play: "FaZe +1.5 vs. Spirit", odds: +115, score: "", game: "CS2", result: "won"},
   { date: "2026-07-31", tournament: "BLAST Bounty", play: "Astralis PK vs. paiN", odds: -135, score: "2-1", game: "CS2"},
   { date: "2026-07-30", tournament: "BLAST Bounty", play: "3DMAX +1.5 vs. MOUZ", odds: +100, score: "1-2", game: "CS2", result: "won"},
   { date: "2026-07-23", tournament: "BLAST Bounty", play: "paiN PK vs. Gentle Mates", odds: -135, score: "2-0", game: "CS2"},
