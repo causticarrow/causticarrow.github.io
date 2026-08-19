81febc8c52ed6871c9867a915c845c7fc5efc89b
causticarrow <rarementx@gmail.com>
2026-08-19 13:00:53 -0400

    Update raw-picks.js

--- raw-picks.js
+++ raw-picks.js
@@ -1,4 +1,5 @@
 window.RAW_PICKS = [
+  { date: "2026-08-20", tournament: "EWC", play: "FaZe +1.5 vs. Vitality", odds: -115, score: "", game: "CS2" },
   { date: "2026-08-20", tournament: "EWC", play: "Falcons -1.5 vs. MongolZ", odds: -110, score: "", game: "CS2" },
   { date: "2026-08-16", tournament: "EWC", play: "Legacy vs. MIBR", odds: -165, score: "2-0", game: "CS2" },
   { date: "2026-08-16", tournament: "EWC", play: "Astralis vs. Ninjas in Pyjamas", odds: -120, score: "2-1", game: "CS2" },
