27b736f61473d8cee9fc256d71aefa7ec1437841
causticarrow <rarementx@gmail.com>
2026-09-09 12:29:17 -0400

    Update raw-picks.js

--- raw-picks.js
+++ raw-picks.js
@@ -1,4 +1,5 @@
 window.RAW_PICKS = [
+  { date: "2026-09-10", tournament: "FISSURE Playground", play: "9z +1.5 vs. G2", odds: -150, score: "", game: "CS2" },
   { date: "2026-09-09", tournament: "FISSURE Playground", play: "MIBR vs. 9z", odds: +160, score: "2-0", game: "CS2" },
   { date: "2026-09-06", tournament: "BLAST Open Porto", play: "Spirit vs. MOUZ", odds: -220, score: "3-1", game: "CS2" },
   { date: "2026-09-05", tournament: "BLAST Open Porto", play: "Spirit vs. Falcons", odds: -160, score: "2-0", game: "CS2" },
